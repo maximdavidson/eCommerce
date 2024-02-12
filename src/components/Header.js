2efe93e00ae762649f508f1f970a0e275650857a
Maxim Davidson <maxd02122004@gmail.com>
2024-02-12 21:09:59 +0300

    Implemented adding items to cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react'
 import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
 
-export default function Header() {
+const showOrders = (props) => {
+   return (
+      <div>
+         {props.orders.map(el => (
+            <Order key={el.id} item={el} />
+         ))}
+      </div>
+   )
+}
+
+const showNothing = () => {
+   return (
+      <div className='empty'>
+         <h2>Товары ещё не добавлены</h2>
+      </div>
+   )
+}
+
+export default function Header(props) {
    let [cartOpen, setCartOpen] = useState(false)
 
    return (
@@ -17,7 +36,8 @@ export default function Header() {
 
             {cartOpen && (
                <div className='shop-cart'>
-
+                  {props.orders.length > 0 ?
+                     showOrders(props) : showNothing()}
                </div>
             )}
          </div>
