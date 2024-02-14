6543c709ac34d546ee8724e42dbea9a15eedd823
Maxim Davidson <maxd02122004@gmail.com>
2024-02-14 20:48:11 +0300

    Items filtering is implemented

---
+++ src/components/Categories.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export class Categories extends Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         categories: [
+            {
+               key: 'all',
+               name: 'Всё'
+            },
+            {
+               key: 'chairs',
+               name: 'Стулья'
+            },
+            {
+               key: 'tables',
+               name: 'Столы'
+            },
+            {
+               key: 'sofas',
+               name: 'Диваны'
+            },
+            {
+               key: 'lamps',
+               name: 'Свет'
+            },
+            {
+               key: 'blankets',
+               name: 'Пледа'
+            },
+         ]
+      }
+   }
+   render() {
+      return (
+         <div className='categories'>
+            {this.state.categories.map(el => (
+               <div key={el.key} onClick={() => this.props.chooseCategory(el.key)}>{el.name}</div>
+            ))}
+         </div>
+      )
+   }
+}
+
+export default Categories
